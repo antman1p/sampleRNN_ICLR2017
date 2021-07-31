6c2e5eccdf861c3ea17126a72e0be63867df0866
Antonio Piazza <antman1p@gmail.com>
2021-07-31 12:50:34 -0700

    created dockerfile, shellscript, and edited 16k.py code

---
+++ datasets/music/docker/new_experiment16k/run_docker_16k.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Usage: sudo ./run_docker_16k <artist name>
+
+artist_name=$1
+mkdir -p $artist_name
+chmod 777 $artist_name
+sed -i "s/chunks/$artist_name/g" Dockerfile
+docker build --no-cache . -t dada-chunk
+sudo docker run --rm --mount type=bind,source="$(pwd)"/$artist_name,target=/dadabots_sampleRNN/datasets/music/$artist_name -ti dada-chunk
+sed -i "s/$artist_name/chunks/g" Dockerfile
